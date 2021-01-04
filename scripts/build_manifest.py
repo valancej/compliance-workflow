3a282a1e2143d828c2fbba66dfa76a9db30eb470
Jeremy <valancejeremy@gmail.com>
2021-01-04 15:21:43 -0700

    updated scripts

--- scripts/build_manifest.py
+++ scripts/build_manifest.py
@@ -19,7 +19,7 @@ from pathlib import Path
 
 def setup_parser():
     parser = argparse.ArgumentParser(description="Tool for generating compliance reports per CI/CD stage")
-    parser.add_argument('-s', '--stage', default='none', help='Pipeline step/stage name. ex. directory, image, registry')
+    parser.add_argument('-s', '--stage', default='none', help='Pipeline step/stage name. ex. directory, image, registry, deploy')
     parser.add_argument('-c', '--compliance', default='cis', help='compliance check to evaluate. ex. cis')
 
     return parser
@@ -49,24 +49,20 @@ def main(arg_parser):
 def process_manifest(content, stage, compliance_standard):
    
     current_time = datetime.datetime.now()
+    git_sha = os.getenv('GITHUB_SHA')
 
     manifest_content = content
     manifest_content["stage"] = stage
     manifest_content["stage_timestamp"] = current_time.strftime("%c")
-    manifest_content["git_sha"] = os.getenv('GITHUB_SHA')
-    #manifest_content["git_sha"] = "sha12349876235"
+    manifest_content["git_sha"] = git_sha
+    manifest_content["full_image_tag"] = content["image_name"] + ":" + git_sha
     manifest_content["type"] = 'compliance_check'
     compliance_checks = {}
     compliance_checks["compliance_standard"] = compliance_standard
 
-    ## Example stepping through the stages of a pipeline
-    # 1. Directory or source code scan using Grype
-    # 2. Scan built image with Grype or Anchore Enterprise
-    # 3. Scan built image in registry with Anchore Enterprise
-    # 4. Evaluate K8s / runtime environment
-
     if stage == 'directory':
        print("directory stage found")
+       compliance_checks["tool"] = 'Grype'
        compliance_checks["compliance_sections"] = {
            '4.4': {
                'description': 'Images should be scanned frequently for any vulnerabilities',
@@ -78,6 +74,8 @@ def process_manifest(content, stage, compliance_standard):
     
     elif stage == 'build':
         print('build stage found')
+        
+        compliance_checks["tool"] = 'Grype'
         compliance_checks["compliance_sections"] = {
             '4.4': {
                 'description': 'Images should be scanned frequently for any vulnerabilities',
@@ -85,27 +83,48 @@ def process_manifest(content, stage, compliance_standard):
             }
         }
         manifest_content.update(compliance_checks)
-        
     
     elif stage == 'registry':
         print('registry stage found')
+        compliance_checks["tool"] = 'Anchore Enterprise'
         compliance_checks["compliance_sections"] = {
+            '4.1': {
+                'description': 'Ensure a container for the user has been created',
+            },
+            '4.2': {
+                'description': 'Ensure containers use trusted base images',
+            },
             '4.3': {
                 'description': 'Ensure that unnecessary packages are not installed in the container',
-                'tool': 'Anchore Enterprise can blacklist packages' 
             },
             '4.4': {
                 'description': 'Images should be scanned frequently for any vulnerabilities',
-                'tool': 'Anchore Enterprise can scan packages at registry level' 
+            },            
+            '4.6': {
+                'description': 'Ensure HEALTHCHECK instructions have been added',
+            },
+            '4.7': {
+                'description': 'Ensure update instructions are not used alone in the Dockerfile',
+            },
+            '4.8': {
+                'description': 'Ensure setuid and setgid permissions are removed',
+            },
+            '4.9': {
+                'description': 'Ensure that COPY is used instead of ADD',
+            },
+            '4.10': {
+                'description': 'Ensure secrets are not stored',
+            },
+            '5.8': {
+                'description': 'Ensure only necessary ports are open',
             }
-
         }
 
         manifest_content.update(compliance_checks)
     
-    elif stage == 'runtime':
-        print('runtime stage found')
-        compliance_checks["compliance_sections"] = ''
+    elif stage == 'deploy':
+        print('deploy stage found')
+        compliance_checks["tool"] = 'Anchore Enterprise'
 
         manifest_content.update(compliance_checks)
 
